004d1aa52368dfc14066608bdde5e76e4056c32b
Rishabh Chauahn <rishabhc32@gmail.com>
2018-06-24 01:21:01 +0530

    added function to correct rendering of unordered list

--- themes/material/static/js/script.js
+++ themes/material/static/js/script.js
@@ -1,1 +1,7 @@
 $(".button-collapse").sideNav();
+
+ulElements = document.getElementsByTagName('ul');
+
+for(let i = 0 ; i < ulElements.length ; i++) {
+    ulElements[i].classList.add('browser-default');
+}
